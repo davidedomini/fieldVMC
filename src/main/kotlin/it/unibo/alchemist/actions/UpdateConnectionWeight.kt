bdfae3f0474c0a4087b20318d38626b1f3fec87c
Angela Cortecchia <angela.cortecchia@unibo.it>
2024-12-11 15:47:27 +0100

    feat: add main weight update logic

--- src/main/kotlin/it/unibo/alchemist/actions/UpdateConnectionWeight.kt
+++ src/main/kotlin/it/unibo/alchemist/actions/UpdateConnectionWeight.kt
@@ -1,25 +1,37 @@
 package it.unibo.alchemist.actions
 
-import it.unibo.alchemist.model.Action
-import it.unibo.alchemist.model.Context
-import it.unibo.alchemist.model.Environment
-import it.unibo.alchemist.model.Node
-import it.unibo.alchemist.model.Position
-import it.unibo.alchemist.model.Reaction
+import it.unibo.alchemist.model.*
 import it.unibo.alchemist.model.actions.AbstractAction
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.collektive.alchemist.device.sensors.SuccessSensor
+import kotlin.math.pow
 
 class UpdateConnectionWeight<T, P : Position<P>>(
     private val environment: Environment<T, P>,
     private val node: Node<T>,
+    val successSensor: SuccessSensor,
+    val constCompetitionRate: Double,
+    val vesselsAdaptationRate: Double,
 ) : AbstractAction<T>(node) {
     override fun cloneAction(
         node: Node<T>,
         reaction: Reaction<T>,
-    ): Action<T> = UpdateConnectionWeight(environment, node)
+    ): Action<T> = UpdateConnectionWeight(environment, node, successSensor, constCompetitionRate, vesselsAdaptationRate)
 
     override fun execute() {
-        TODO("Not yet implemented")
+        // todo check on the clock of the node
+        environment.getNeighborhood(node).filter { n -> // children
+            n.getConcentration(SimpleMolecule("parent")) == node.id
+        }.forEach { n ->
+            val weight = n.getConcentration(SimpleMolecule("weight")) as Double
+            val success = n.getConcentration(SimpleMolecule("success")) as Double
+            val newWeight = weight + vesselsAdaptationRate * (success.pow(competition()) - weight)
+            n.setConcentration(SimpleMolecule("weight"), newWeight as T)
+        }
     }
 
     override fun getContext(): Context? = Context.NEIGHBORHOOD
+
+    private fun competition(): Double =
+        constCompetitionRate + successSensor.sensorCompetitionRate * successSensor.getLocalSuccess()
 }
