742af1aa31510b3326ba3e4dcb35a92996de3bb4
Angela Cortecchia <angela.cortecchia@unibo.it>
2024-12-16 16:20:25 +0100

    feat: add check on execution clock

--- src/main/kotlin/it/unibo/alchemist/actions/EvaluateSuccess.kt
+++ src/main/kotlin/it/unibo/alchemist/actions/EvaluateSuccess.kt
@@ -1,10 +1,18 @@
 package it.unibo.alchemist.actions
 
-import it.unibo.alchemist.model.*
+import it.unibo.alchemist.model.Action
+import it.unibo.alchemist.model.Context
+import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Node.Companion.asProperty
+import it.unibo.alchemist.model.Position
+import it.unibo.alchemist.model.Reaction
 import it.unibo.alchemist.model.actions.AbstractAction
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.collektive.alchemist.device.properties.Clock
+import it.unibo.collektive.alchemist.device.properties.Cycle.BACKWARD
+import it.unibo.collektive.alchemist.device.properties.Cycle.FORWARD
+import it.unibo.collektive.alchemist.device.properties.Cycle.SPAWNED
 import it.unibo.collektive.alchemist.device.properties.impl.ExecutionClockProperty
 import it.unibo.collektive.alchemist.device.sensors.impl.SuccessSensorProperty
 import kotlin.math.max
@@ -13,46 +21,63 @@ import kotlin.math.min
 class EvaluateSuccess<T, P : Position<P>>(
     private val environment: Environment<T, P>,
     private val node: Node<T>,
-    val clock: ExecutionClockProperty<T, P>,
+    private val clock: ExecutionClockProperty<T, P>,
     private val successSensor: SuccessSensorProperty<T, P>,
-    val constProductionRate: Double,
-    val constTransferRate: Double,
-    val sensorProductionRate: Double,
-    val sensorTransferRate: Double,
+    private val constProductionRate: Double,
+    private val constTransferRate: Double,
+    private val sensorProductionRate: Double,
+    private val sensorTransferRate: Double,
 ) : AbstractAction<T>(node) {
     override fun cloneAction(
         node: Node<T>,
         reaction: Reaction<T>,
-    ): Action<T> = EvaluateSuccess(environment, node, clock, successSensor, constProductionRate, constTransferRate, sensorProductionRate, sensorTransferRate)
+    ): Action<T> =
+        EvaluateSuccess(
+            environment,
+            node,
+            clock,
+            successSensor,
+            constProductionRate,
+            constTransferRate,
+            sensorProductionRate,
+            sensorTransferRate,
+        )
 
     override fun execute() {
-        // todo add check on the turn of the node
-//        val clock = node.properties.first { it is ClockProperty<*, *> }
-//        val nbr = environment.getNeighborhood(node).map { n -> n.asProperty() }
-
-//        clock.currentClock()
-        if(node.getConcentration(SimpleMolecule("leaf")) == true) {
-            val localProduction = max(0.0, production())
-            successSensor.setLocalSuccess(localProduction)
-            successSensor.setSuccess(localProduction)
-        } else if(node.getConcentration(SimpleMolecule("intermediate")) == true) {
-            val childrenSuccessSum = environment.getNeighborhood(node).filter { n ->
-                n.getConcentration(SimpleMolecule("parent")) == node.id
-            }.sumOf { n ->
-                n.getConcentration(SimpleMolecule("success")) as Double
+        val allNodes = environment.nodes.map { it to it.asProperty<T, ExecutionClockProperty<T, P>>() }
+        val children = allNodes.filter { (node, _) -> node.getConcentration(SimpleMolecule("parent")) == node.id }
+        val current = clock.currentClock()
+        val nodesInBackward = allNodes.filter { (_, nodesClock) -> nodesClock.currentClock().action == BACKWARD }
+        when {
+            node.getConcentration(SimpleMolecule("leaf")) == true -> {
+                if ((current.action == FORWARD || current.action == SPAWNED) && nodesInBackward.isEmpty()) {
+                    val localProduction = max(0.0, production())
+                    successSensor.setLocalSuccess(localProduction)
+                    successSensor.setSuccess(localProduction)
+                    clock.nextClock()
+                }
             }
-            val success = max(0.0, min(1.0, transfer() * childrenSuccessSum))
-            successSensor.setSuccess(success)
-        } else {
-            throw IllegalStateException("Node is not a leaf or an intermediate node")
+            node.getConcentration(SimpleMolecule("intermediate")) == true || node.getConcentration(SimpleMolecule("root")) == true -> {
+                val childrenInBackward =
+                    children.filter { (_, nodesClock) ->
+                        nodesClock.currentClock() == Clock(time = current.time + 1, action = BACKWARD)
+                    }
+                if (childrenInBackward.isNotEmpty() && children.isNotEmpty() && childrenInBackward.size == children.size) {
+                    val childrenSuccessSum =
+                        children.sumOf { (n, _) ->
+                            n.getConcentration(SimpleMolecule("success")) as Double
+                        }
+                    val success = max(0.0, min(1.0, transfer() * childrenSuccessSum))
+                    successSensor.setSuccess(success)
+                }
+            }
+            else -> throw IllegalStateException("Node is in a inconsistent state")
         }
     }
 
     override fun getContext(): Context? = Context.NEIGHBORHOOD
 
-    private fun production(): Double =
-        constProductionRate + sensorProductionRate * successSensor.getLocalSuccess()
+    private fun production(): Double = constProductionRate + sensorProductionRate * successSensor.getLocalSuccess()
 
-    private fun transfer(): Double =
-        constTransferRate + sensorTransferRate * successSensor.getLocalSuccess()
+    private fun transfer(): Double = constTransferRate + sensorTransferRate * successSensor.getLocalSuccess()
 }
