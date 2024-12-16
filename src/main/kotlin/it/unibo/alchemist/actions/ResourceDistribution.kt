0ee247a17c0c6a896131edd6dc823bd93e5f554e
Angela Cortecchia <angela.cortecchia@unibo.it>
2024-12-16 18:33:33 +0100

    fix: add check on node id

--- src/main/kotlin/it/unibo/alchemist/actions/ResourceDistribution.kt
+++ src/main/kotlin/it/unibo/alchemist/actions/ResourceDistribution.kt
@@ -12,6 +12,7 @@ import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.collektive.alchemist.device.properties.Clock
 import it.unibo.collektive.alchemist.device.properties.Cycle.BACKWARD
 import it.unibo.collektive.alchemist.device.properties.Cycle.FORWARD
+import it.unibo.collektive.alchemist.device.properties.Cycle.SPAWNED
 import it.unibo.collektive.alchemist.device.properties.impl.ExecutionClockProperty
 import it.unibo.collektive.alchemist.device.sensors.impl.ResourceSensorProperty
 import it.unibo.collektive.alchemist.device.sensors.impl.SuccessSensorProperty
@@ -46,9 +47,13 @@ class ResourceDistribution<T, P : Position<P>>(
 
     override fun execute() {
         val allNodes = environment.nodes.map { it to it.asProperty<T, ExecutionClockProperty<T, P>>() }
-        val parent = allNodes.find { (n, _) -> node.id == n.getConcentration(SimpleMolecule("parent")) }
+        val parent = allNodes
+            .filterNot { (n, _) -> n.id == node.id }
+            .firstOrNull { (n, _) -> node.id == n.getConcentration(SimpleMolecule("parent")) }
         val current = clock.currentClock()
-        val nodesNotInBackward = allNodes.filterNot { (_, nodesClock) -> nodesClock.currentClock().action == BACKWARD }
+        val nodesNotInBackward = allNodes
+            .filterNot { (_, nodesClock) -> nodesClock.currentClock().action == BACKWARD || nodesClock.currentClock().action == SPAWNED }
+            .filterNot { (n, _) -> n.id == node.id }
         if( (parent == null && nodesNotInBackward.isEmpty()) ||
             (parent!!.second.currentClock() == Clock(time = current.time, action = FORWARD)) ) {
             var availableResources = node.getConcentration(SimpleMolecule("resource")) as Double
@@ -89,3 +94,54 @@ class ResourceDistribution<T, P : Position<P>>(
 
     private fun competition(): Double = constCompetitionRate + sensorCompetitionRate * successSensor.getLocalSuccess()
 }
+
+/*
+
+//        when {
+//            parent == null && nodesNotInBackward.isEmpty() -> {
+//                var availableResources = node.getConcentration(SimpleMolecule("resource")) as Double
+//                availableResources = availableResources + resourceSensor.maxResource
+//                node.setConcentration(SimpleMolecule("resource"), availableResources as T)
+//                val children = allNodes.filter { (n, _) ->
+//                    n.getConcentration(SimpleMolecule("parent")) == node.id
+//                }
+//                clock.nextClock()
+//                if (children.isNotEmpty()) {
+//                    val weightSum = children.sumOf { (n, _) ->
+//                        n.getConcentration(SimpleMolecule("weight")) as Double
+//                    }
+//                    children.forEach { (n, _) ->
+//                        val weight = n.getConcentration(SimpleMolecule("weight")) as Double
+//                        val success = n.getConcentration(SimpleMolecule("success")) as Double
+//                        val newWeight = weight + vesselsAdaptationRate * (success.pow(competition()) - weight)
+//                        n.setConcentration(SimpleMolecule("weight"), newWeight as T)
+//                        val resource = availableResources * (weight / weightSum)
+//                        n.setConcentration(SimpleMolecule("resource"), resource as T)
+//                    }
+//                }
+//            }
+//            parent != null && parent.second.currentClock() == Clock(time = current.time, action = FORWARD) -> {
+//                var availableResources = node.getConcentration(SimpleMolecule("resource")) as Double
+//                val remainingResources = availableResources - constConsumptionRate
+//                node.setConcentration(SimpleMolecule("resource"), remainingResources as T)
+//                val children = allNodes.filter { (n, _) ->
+//                    n.getConcentration(SimpleMolecule("parent")) == node.id
+//                }
+//                clock.nextClock()
+//                if (children.isNotEmpty()) {
+//                    val weightSum = children.sumOf { (n, _) ->
+//                        n.getConcentration(SimpleMolecule("weight")) as Double
+//                    }
+//                    children.forEach { (n, _) ->
+//                        val weight = n.getConcentration(SimpleMolecule("weight")) as Double
+//                        val success = n.getConcentration(SimpleMolecule("success")) as Double
+//                        val newWeight = weight + vesselsAdaptationRate * (success.pow(competition()) - weight)
+//                        n.setConcentration(SimpleMolecule("weight"), newWeight as T)
+//                        val resource = remainingResources * (weight / weightSum)
+//                        n.setConcentration(SimpleMolecule("resource"), resource as T)
+//                    }
+//                }
+//            }
+//            else -> throw IllegalStateException("Node is in an inconsistent state")
+//        }
+ */
