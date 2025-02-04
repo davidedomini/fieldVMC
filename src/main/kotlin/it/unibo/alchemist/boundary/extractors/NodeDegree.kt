44d6d49cf53e26c374e264ddf2917b3c19247c2d
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-04 17:21:40 +0100

    fix: implement by using Alchemist's new `AbstractAggregatingDoubleExporter`

--- src/main/kotlin/it/unibo/alchemist/boundary/extractors/NodeDegree.kt
+++ src/main/kotlin/it/unibo/alchemist/boundary/extractors/NodeDegree.kt
@@ -4,11 +4,11 @@ import it.unibo.alchemist.boundary.ExportFilter
 import it.unibo.alchemist.boundary.exportfilters.CommonFilters
 import it.unibo.alchemist.model.Actionable
 import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Time
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.alchemist.util.StatUtil
 import org.apache.commons.math3.stat.descriptive.UnivariateStatistic
-import kotlin.math.sqrt
 
 class NodeDegree
     @JvmOverloads
@@ -17,64 +17,42 @@ class NodeDegree
         private val filter: ExportFilter = CommonFilters.ONLYFINITE.filteringPolicy,
         aggregatorNames: List<String> = listOf("max", "mean", "stdev"),
         precision: Int = 2,
-    ) : AbstractDoubleExporter(precision) {
-        private companion object {
-            private const val NAME = "network-degree"
-        }
-
-        private val aggregators: Map<String, UnivariateStatistic> =
-            aggregatorNames
-                .associateWith { StatUtil.makeUnivariateStatistic(it) }
-                .filter { it.value.isPresent }
-                .map { it.key to it.value.get() }
-                .toMap()
-
-        override val columnNames: List<String> =
-            aggregators.keys
-                .takeIf { it.isNotEmpty() }
-                ?.map { "$NAME[$it]" }
-                ?: listOf("$NAME@node-id")
-
-        fun <T> calculateDegreeStatistics(environment: Environment<T, *>): Map<String, Double> {
-            val degrees = environment.nodes.map { node ->
-                val neighbors = environment.getNeighborhood(node)
-                if(checkChildren) {
-                    neighbors.filter { n ->
-                        n.getConcentration(SimpleMolecule("parent")) == node.id ||
-                            node.getConcentration(SimpleMolecule("parent")) == n.id
-                    }.size
-                } else {
-                    neighbors.size()
-                }
-            }
-            val maxDegree = degrees.maxOrNull()?.toDouble() ?: 0.0
-            val averageDegree = degrees.average()
-            val variance =
-                degrees.fold(0.0) { acc, degree ->
-                    acc + (degree - averageDegree) * (degree - averageDegree)
-                } / degrees.size
-            val stdDeviation = sqrt(variance)
-
-            return mapOf(
-                "$NAME.max" to maxDegree,
-                "$NAME.average" to averageDegree,
-                "$NAME.std_deviation" to stdDeviation,
-            )
-        }
-
-        override fun <T> extractData(
-            environment: Environment<T, *>,
-            reaction: Actionable<T>?,
-            time: Time,
-            step: Long,
-        ): Map<String, Double> = calculateDegreeStatistics(environment)
-//        val stats = calculateDegreeStatistics(environment)
-//        val filtered = environment.nodes
-//            .map { n -> environment.getNeighborhood(n).size() }
-//            .flatMap { t -> filter.apply(t.toDouble()) }
-//            .toDoubleArray()
-//        return aggregators.map { (aggregatorName, aggregator) ->
-//            "$singleColumnName[$aggregatorName]" to aggregator.evaluate(filtered)
-//        }.toMap()
-//    }
+    ) : AbstractAggregatingDoubleExporter(filter, aggregatorNames, precision) {
+    private companion object {
+        private const val NAME = "network-degree"
     }
+
+    override val colunmName: String
+        get() = NAME
+
+    private val aggregators: Map<String, UnivariateStatistic> =
+        aggregatorNames
+            .associateWith { StatUtil.makeUnivariateStatistic(it) }
+            .filter { it.value.isPresent }
+            .map { it.key to it.value.get() }
+            .toMap()
+
+    override val columnNames: List<String> =
+        aggregators.keys
+            .takeIf { it.isNotEmpty() }
+            ?.map { "$NAME[$it]" }
+            ?: listOf("$NAME@node-id")
+
+    override fun <T> getData(
+        environment: Environment<T, *>,
+        reaction: Actionable<T>?,
+        time: Time,
+        step: Long
+    ): Map<Node<T>, Double> =
+        environment.nodes.associateWith { node ->
+            val neighbors = environment.getNeighborhood(node)
+            if(checkChildren) {
+                neighbors.filter { n ->
+                    n.getConcentration(SimpleMolecule("parent")) == node.id ||
+                        node.getConcentration(SimpleMolecule("parent")) == n.id
+                }.size
+            } else {
+                neighbors.size()
+            }.toDouble()
+        }
+}
