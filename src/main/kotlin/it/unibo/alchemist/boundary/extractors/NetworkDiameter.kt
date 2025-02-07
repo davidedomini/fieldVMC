5e9e60d27b01ab27dcafa875313a446ac4f4e654
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-07 11:58:26 +0100

    feat: use alchemist's network diameter function

--- src/main/kotlin/it/unibo/alchemist/boundary/extractors/NetworkDiameter.kt
+++ src/main/kotlin/it/unibo/alchemist/boundary/extractors/NetworkDiameter.kt
@@ -1,57 +1,30 @@
 package it.unibo.alchemist.boundary.extractors
 
-import it.unibo.alchemist.boundary.Extractor
+import it.unibo.alchemist.boundary.ExportFilter
 import it.unibo.alchemist.model.Actionable
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Time
+import it.unibo.alchemist.util.Environments.networkDiameterByHopDistance
 
-class NetworkDiameter() : Extractor<Int> {
-    override val columnNames = listOf(NAME)
+class NetworkDiameter
+    @JvmOverloads
+    constructor(
+        filter: ExportFilter,
+        aggregators: List<String>,
+        precision: Int = 2,
+    ) : AbstractAggregatingDoubleExporter(filter, aggregators, precision) {
+        private companion object {
+            private const val NAME: String = "network-diameter"
+        }
+        override val columnName: String = NAME
 
-    override fun <T> extractData(
-        environment: Environment<T, *>,
-        reaction: Actionable<T>?,
-        time: Time,
-        step: Long,
-    ): Map<String, Int> {
-        val diameter = findDiameter(environment)
-        return mapOf(NAME to diameter)
-    }
-
-    private companion object {
-        private const val NAME: String = "network-diameter"
-    }
-
-    fun <T> findDiameter(environment: Environment<T, *>): Int {
-        fun bfs(start: Node<T>): Map<Node<T>, Int> {
-            val distances = mutableMapOf<Node<T>, Int>()
-            val queue: ArrayDeque<Node<T>> = ArrayDeque()
-
-            distances[start] = 0
-            queue.add(start)
-
-            while (queue.isNotEmpty()) {
-                val current = queue.removeFirst()
-                val currentDistance = distances[current] ?: 0
-
-                for (neighbor in environment.getNeighborhood(current)) {
-                    if (neighbor !in distances) {
-                        distances[neighbor] = currentDistance + 1
-                        queue.add(neighbor)
-                    }
-                }
+        override fun <T> getData(
+            environment: Environment<T, *>,
+            reaction: Actionable<T>?,
+            time: Time,
+            step: Long
+        ): Map<Node<T>, Double> = environment.networkDiameterByHopDistance().let { diameter ->
+                environment.nodes.associateWith { diameter }
             }
-            return distances
-        }
-
-        var diameter = 0
-        for (node in environment.nodes) {
-            val distances = bfs(node)
-            val maxDistanceFromNode = distances.values.maxOrNull() ?: 0
-            diameter = maxOf(diameter, maxDistanceFromNode)
-        }
-
-        return diameter
     }
-}
