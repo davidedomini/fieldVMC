f73ad7d2ce0434e21cd63f0654a4b9e0504baee6
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-07 15:05:35 +0100

    fix: update values for network density evaluation

--- src/main/kotlin/it/unibo/alchemist/boundary/extractors/NetworkDensity.kt
+++ src/main/kotlin/it/unibo/alchemist/boundary/extractors/NetworkDensity.kt
@@ -10,7 +10,7 @@ import kotlin.Double.Companion.NaN
 class NetworkDensity
     @JvmOverloads
     constructor(
-        private val filter: ExportFilter,
+        filter: ExportFilter,
         aggregators: List<String>,
         precision: Int = 2,
     ) : AbstractAggregatingDoubleExporter(filter, aggregators, precision) {
@@ -26,25 +26,21 @@ class NetworkDensity
             time: Time,
             step: Long,
         ): Map<Node<T>, Double> {
-            val outers: MutableMap<String, Double> =
+            var outers: MutableMap<String, Double> =
                 listOf<String>("top", "bottom", "right", "left")
                     .associateWith { NaN }
                     .toMutableMap()
             return environment.nodes.associateWith { n ->
                 val nodePos = environment.getPosition(n).coordinates
-                val right = outers["right"]!!
-                val left = outers["left"]!!
-                val top = outers["top"]!!
-                val bottom = outers["bottom"]!!
-                when {
-                    right.isNaN() || nodePos[0] > right -> outers["right"] = nodePos[0]
-                    left.isNaN() || nodePos[0] < left -> outers["left"] = nodePos[0]
-                    top.isNaN() || nodePos[1] > top -> outers["top"] = nodePos[1]
-                    bottom.isNaN() || nodePos[1] < bottom -> outers["bottom"] = nodePos[1]
-                    else -> {
+                outers = outers.mapValues { (key, value) ->
+                    when {
+                        key == "right" && (value.isNaN() || nodePos[0] > value) -> nodePos[0]
+                        key == "left" && (value.isNaN() || nodePos[0] < value) -> nodePos[0]
+                        key == "top" && (value.isNaN() || nodePos[1] > value) -> nodePos[1]
+                        key == "bottom" && (value.isNaN() || nodePos[1] < value) -> nodePos[1]
+                        else -> value
                     }
-                }
-
+                } as MutableMap<String, Double>
                 // Calculate the area of the rectangle given by the outermost nodes
                 val area = (outers["right"]!! - outers["left"]!!) * (outers["top"]!! - outers["bottom"]!!)
                 environment.nodeCount / area
