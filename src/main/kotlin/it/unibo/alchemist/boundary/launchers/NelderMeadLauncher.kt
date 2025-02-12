f35463774e52729e579812501b94bad44d5c2c36
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-12 18:36:21 +0100

    feat: final way to create the symplex

--- src/main/kotlin/it/unibo/alchemist/boundary/launchers/NelderMeadLauncher.kt
+++ src/main/kotlin/it/unibo/alchemist/boundary/launchers/NelderMeadLauncher.kt
@@ -2,8 +2,8 @@ package it.unibo.alchemist.boundary.launchers
 
 import it.unibo.alchemist.boundary.Launcher
 import it.unibo.alchemist.boundary.Loader
-import it.unibo.alchemist.boundary.Variable
-import it.unibo.common.DataRetriever.retrieveData
+import it.unibo.common.DataRetriever.meanOnCleanedData
+import org.danilopianini.rrmxmx.RrmxmxRandom
 import java.nio.file.Paths
 import kotlin.math.pow
 
@@ -14,8 +14,9 @@ constructor(
     private val metrics: List<String> = emptyList(),
     private val maxBatchSize: Double = 0.0,
     private val autostart: Boolean = false,
-    private val showProgress: Boolean = false,
     private val parallelism: Int = Runtime.getRuntime().availableProcessors(),
+    private val showProgress: Boolean = false,
+    private val seed: ULong = RrmxmxRandom.DEFAULT_SEED,
 ) : Launcher {
     companion object {
         val experiments = listOf("classic-vmc", "fixed-leader")
@@ -27,7 +28,7 @@ constructor(
 
     val objFunctionClassicVMC =
         geometricMean(
-            retrieveData(
+            meanOnCleanedData(
                 listOf(experiments.first()),
                 path,
             ).filter { (key, _) -> key.removePrefix("${experiments.first()}@") in metrics }
@@ -37,34 +38,26 @@ constructor(
 
     @Synchronized
     override fun launch(loader: Loader) {
-        val instances: Map<String, Variable<Double>> = loader.variables.filter { it.key in variables }.map { it.key to it.value as Variable<Double> }.toMap()
-        if (instances.isEmpty()) {
-            throw IllegalArgumentException("No matching variables found.")
+        require(loader.variables.isNotEmpty() || variables.isNotEmpty()) {
+            "No variables found, can not optimize anything."
         }
-        // I want to fill the initial symplex variables.size + 1 times,
-        // so that I can have a symplex with variables.size + 1 vertices
-        // each vertex will have a different value for each variable,
-        // if the iteration is the first, I take the min value for each variable
-        // if the iteration is the last, I take the max value for each variable
-        // if the iteration is in between, I take the default value for each variable
-        // otherwise, I take the value of the variable at the index of the iteration
-        // variables can have different lengths
-        val keys = instances.keys.toList()
-        val variablesToKey = keys.map { key -> instances.getValue(key) }
-        val minMax: List<Pair<Double, Double>> = variablesToKey.map { variable ->
-            val max = variable.stream().toList().max()
-            val min = variable.stream().toList().min()
-            (min to max)
+        // I need that the variables passed through the loader are numbers
+        val randomGenerator = RrmxmxRandom(seed)
+        val instances: Map<String, ClosedRange<Double>> = variables.associateWith { varName ->
+            val variable = loader.variables.getValue(varName)
+            val allValues = variable.stream().map {
+                check(it is Number) { "All variables to optimize must be Numbers. $varName has value $it." }
+                it.toDouble()
+            }.toList()
+            allValues.min()..allValues.max()
         }
-        val allMin = minMax.map { it.first }
-        val allMax = minMax.map { it.second }
-        // generate all solution which mix min and max values
-        val simplexVertex = mutableListOf<List<Double>>(allMin)
-        for (i in 0 until variables.size) {
-            val left = allMin.subList(0, i)
-            val right = allMax.subList(i + 1, allMax.size)
-            simplexVertex.add(left + allMax[i] + right)
+        val symplexVertices = (0..variables.size + 1).map {
+            instances.mapValues { (_, range) -> randomGenerator.nextDouble(range.start, range.endInclusive) }
         }
+
+        // now I need to run #seed times the simulations, with different seeds
+        // the generated results will be used in the objective function to optimize
+
     }
 
     private fun goalFunction(objective: Double): (Collection<Double>) -> Double = { toOptimize ->
