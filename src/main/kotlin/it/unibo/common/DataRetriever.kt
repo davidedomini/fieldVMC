16a320abd89f712748d2a5aeab0e706e535ea118
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-13 19:40:34 +0100

    docs: add missing documentation

--- src/main/kotlin/it/unibo/common/DataRetriever.kt
+++ src/main/kotlin/it/unibo/common/DataRetriever.kt
@@ -2,7 +2,23 @@ package it.unibo.common
 
 import java.io.File
 
+/**
+ * This object is responsible for retrieving data from the simulation files.
+ * It can retrieve data from the original files or from the cleaned files.
+ * The cleaned files are created by removing the comments and the empty lines from the original files.
+ * The data is then stored in a map where the key is the experiment name and the column name separated by a '@'.
+ * The value is the mean value of the column for the experiment.
+ * The mean value is computed by averaging the values of the last row of each file.
+ */
 object DataRetriever {
+
+    /**
+     * Retrieve data from the simulation files.
+     * The data is stored in a map where the key is the experiment name and the column name separated by a '@'.
+     * The value is the mean value of the column for the [experiments] at the given [path].
+     * The mean value is computed by averaging the values of the last row of each file.
+     * The data is also written to a cleaned file for each experiment.
+     */
     fun retrieveData(
         experiments: List<String>,
         path: String,
@@ -30,11 +46,19 @@ object DataRetriever {
         return data
     }
 
+
+    /**
+     * Retrieves the list of files for a given [experiment] in the specified [path].
+     * The files must start with the experiment name and end with ".csv".
+     */
     private fun getExperimentFiles(
         path: String,
         experiment: String,
     ): Array<File> = File(path).listFiles { _, name -> name.startsWith(experiment) && name.endsWith(".csv") } ?: emptyArray()
 
+    /**
+     * Processes the given [file] and returns the header and the last row of the data.
+     */
     private fun processFile(file: File): Pair<List<String>, List<String>> {
         val lines = file.readLines()
         val dataStartIndex = lines.indexOfFirst { it.startsWith("# time") }
@@ -58,6 +82,13 @@ object DataRetriever {
         return Pair(header, lastRow)
     }
 
+    /**
+     * Writes the cleaned data to a new file for the given [experiment] in the specified [path].
+     * The output file will be named `cleaned_<experiment>.csv`.
+     * The first row of the file will contain the [columnNames],
+     * and each subsequent row will contain the last row of data from each file,
+     * passed as [collectedRows].
+     */
     private fun writeCleanedData(
         path: String,
         experiment: String,
@@ -77,6 +108,11 @@ object DataRetriever {
         outputFile.writeText(outputContent)
     }
 
+    /**
+     * Computes the mean value for each column in the given [experiment] data.
+     * The mean value is computed by averaging the values of the last row of each [columnNames]
+     * in the [collectedRows].
+     */
     private fun computeMeanValues(
         experiment: String,
         columnNames: List<String>,
@@ -88,6 +124,10 @@ object DataRetriever {
                 "$experiment@$column" to meanValue
             }.toMap()
 
+    /**
+     * Computes the mean value for each column in the cleaned data files for the given [experiments] at the specified [path].
+     * The mean value is computed by averaging the values of the last row of each file.
+     */
     fun meanOnCleanedData(
         experiments: List<String>,
         path: String,
