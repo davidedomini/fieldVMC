fbeccdb0eaafc93f4463a282e4e9748dab303ec5
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-02-14 11:45:19 +0100

    refactor: rename target function and add docs

--- src/main/kotlin/it/unibo/Objective.kt
+++ src/main/kotlin/it/unibo/Objective.kt
@@ -7,15 +7,30 @@ import java.nio.file.Paths
 import kotlin.Double.Companion.NaN
 import kotlin.math.pow
 
-fun geometricMean(metrics: Collection<Double>): Double = metrics.fold(1.0) { acc, next -> acc * next.pow(2) }.pow(1.0 / metrics.size)
+/**
+ * Compute the geometric mean of a collection of [metrics].
+ * The geometric mean is the n-th root of the product of the metrics.
+ * The n-th root is computed by raising the product to the power of 1/n.
+ */
+fun geometricMean(metrics: Collection<Double>): Double =
+    metrics.fold(1.0) { acc, next -> acc * next }.pow(1.0 / metrics.size)
 
-fun goal(): Double {
+/**
+ * The target value for the optimization.
+ * It is the geometric mean of the mean values of the metrics for the classic-vmc experiment.
+ * The metrics are:
+ * - number of nodes;
+ * - x and y coordinates of the network hub;
+ * - network diameter;
+ * - network degree.
+ */
+fun target(): Double {
     val metrics = listOf(
+        "classic-vmc@nodes",
         "classic-vmc@network-hub-xCoord",
         "classic-vmc@network-hub-yCoord",
         "classic-vmc@network-diameter[mean]",
-//        "classic-vmc@network-density[mean]",
-        "classic-vmc@network-degree[mean]",
+        "classic-vmc@nodes-degree[mean]",
     )
     return geometricMean(
         meanOnCleanedData(listOf("classic-vmc"), Paths.get("").toAbsolutePath().toString() + "/data")
@@ -23,25 +38,43 @@ fun goal(): Double {
     )
 }
 
+/**
+ * The goal for the optimization.
+ */
 class Goal : (Environment<*, *>) -> Double {
     override fun invoke(env: Environment<*, *>): Double = env.minimize()
 }
 
+/**
+ * The function to minimize.
+ * It is the difference between the target value and the current value.
+ * The current value is the geometric mean of the metrics for the given [Environment].
+ * The difference is taken modulo 2 to avoid negative values.
+ * The metric values are:
+ * - number of nodes;
+ * - x and y coordinates of the network hub;
+ * - network diameter;
+ * - network degree.
+ */
 fun Environment<*, *>.minimize(): Double =
     networkHub().let { (xCoord, yCoord) ->
         val current =
             geometricMean(
                 listOf(
-                    networkDiameterByHopDistance(),
-                    networkDegree(),
                     nodeCount.toDouble(),
                     xCoord,
                     yCoord,
+                    networkDiameterByHopDistance(),
+                    nodesDegree(),
                 ),
             )
-        (goal() - current).mod(2.0)
+        (target() - current).mod(2.0)
     }
 
+/**
+ * The network hub coordinates.
+ * The x and y coordinates are the average of the x and y coordinates of the nodes in the network.
+ */
 fun <T> Environment<T, *>.networkHub(): Pair<Double, Double> =
     fold(0.0 to 0.0) { acc, next ->
         val nodePos = this.getPosition(next)
@@ -50,6 +83,24 @@ fun <T> Environment<T, *>.networkHub(): Pair<Double, Double> =
         sum.first / this.nodeCount to sum.second / this.nodeCount
     }
 
+/**
+ * The degree of the nodes in the network.
+ * The degree of a node is the number of neighbors it has.
+ * The result is the average of the degrees of all the nodes in the network.
+ */
+fun <T> Environment<T, *>.nodesDegree(): Double =
+    nodes
+        .map { n ->
+            val neighbors = getNeighborhood(n)
+            neighbors.size().toDouble()
+        }.average()
+
+/**
+ * The network density.
+ * The network density is the average number of nodes per unit area.
+ * The area is the rectangle given by the outermost nodes in the network.
+ * The result is the average of the network densities of all the nodes in the network.
+ */
 fun <T> Environment<T, *>.networkDensity(): Double {
     var outers: MutableMap<String, Double> =
         listOf<String>("top", "bottom", "right", "left")
@@ -73,10 +124,3 @@ fun <T> Environment<T, *>.networkDensity(): Double {
             nodeCount / area
         }.average()
 }
-
-fun <T> Environment<T, *>.networkDegree(): Double =
-    nodes
-        .map { n ->
-            val neighbors = getNeighborhood(n)
-            neighbors.size().toDouble()
-        }.average()
