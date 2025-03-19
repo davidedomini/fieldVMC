cb93cb3fa650bef41495747fb2082efed5ea7dcf
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-03-19 15:33:09 +0100

    perf: improve subnetwork diameter evaluation by checking whether a node is the father of another node

--- src/main/kotlin/it/unibo/alchemist/boundary/extractors/SubnetworksDiameter.kt
+++ src/main/kotlin/it/unibo/alchemist/boundary/extractors/SubnetworksDiameter.kt
@@ -5,7 +5,8 @@ import it.unibo.alchemist.model.Actionable
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Time
-import it.unibo.alchemist.util.Environments.allSubNetworksByNodeWithHopDistance
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.alchemist.util.Environments.allSubNetworksByNode
 
 class SubnetworksDiameter
 @JvmOverloads
@@ -16,6 +17,14 @@ constructor(
 ) : AbstractAggregatingDoubleExporter(filter, aggregators, precision) {
     private companion object {
         private const val NAME: String = "subnetworks-diameter"
+        private operator fun Node<*>.get(name: String) = getConcentration(SimpleMolecule(name))
+        private val Node<*>.parent: Int
+            get() = when (val parent: Any? = get("parent")) {
+                is Int -> parent
+                is Number -> parent.toInt()
+                is String -> parent.toInt()
+                else -> error("Parent value not yet supported.")
+            }
     }
 
     override val columnName: String = NAME
@@ -26,5 +35,7 @@ constructor(
         time: Time,
         step: Long,
     ): Map<Node<T>, Double> =
-        environment.allSubNetworksByNodeWithHopDistance().mapValues { (_, subnet) -> subnet.diameter }
+        environment
+            .allSubNetworksByNode { n1, n2 -> if (n1.parent == n2.id) 1.0 else 0.0 }
+            .mapValues { (_, subnet) -> subnet.diameter }
 }
